87c4a03d203c4eaeb7574132a6c81564da1692f0
dlq <dailiqiang_6@163.com>
2018-12-29 11:47:28 +0800

    更新工具+项目

--- src/components/robot.vue
+++ src/components/robot.vue
@@ -1,35 +1,6 @@
 <template>
      <div id="container">
-<<<<<<< HEAD
-        <div class="header">
-            <span style="float: left;">黑聊,专注于舒缓情绪</span>
-            <span style="float: right;">今天</span>
-        </div>
-        <ul class="content">
-            <!-- 如果boy -->
-            <li v-for="(item, index) in messageList" :key="index">
-                <img :src="item.isBoy?'./static/img/162.png':'./static/img/162.png'" :class="item.isBoy?'imgright':'imgleft'" /><span
-                    :class="item.isBoy?'spanright':'spanleft'">{{
-                    item.message
-                    }}</span>
-            </li>
-            <!-- 如果女 -->
-            <!--
-          <li>
-            <img src="./img/girl.png" class="imgleft" /><span class="spanleft"
-              >好你个鬼</span
-            >
-          </li>
-        -->
-        </ul>
-        <div class="footer">
-            <div id="user_face_icon"><img src="lib/boy.jpg" alt="" /></div>
-            <input v-model.trim="message" id="text" type="text" @keyup.enter="send" placeholder="说点什么吧..." />
-            <span @click="send" id="btn">发送</span>
-        </div>
-=======
        <iframe :src="src"></iframe>
->>>>>>> master
     </div>
 </template>
 
@@ -38,10 +9,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-<<<<<<< HEAD
-=======
       src: 'http://wogeshi.lidongliang4444.cn/04tulingRobot/',
->>>>>>> master
       toolArr:[],//资讯
       input:'',
         // 聊天数组
@@ -98,120 +66,13 @@ export default {
         overflow: hidden;
       }
       #container {
-        width: 450px;
-        height: 780px;
-        background: #eee;
-        margin: 20px auto 0;
-        position: relative;
-        box-shadow: 20px 20px 55px #777;
-<<<<<<< HEAD
-      }
-      .header {
-        background: #000;
-        height: 40px;
-        color: #fff;
-        line-height: 34px;
-        font-size: 20px;
-        padding: 0 10px;
-      }
-      .footer {
-        width: 430px;
-        height: 50px;
-        background: #666;
-        position: absolute;
-        bottom: 0;
-        padding: 10px;
-      }
-      .footer input {
-        width: 275px;
-        height: 45px;
-        outline: none;
-        font-size: 20px;
-        text-indent: 10px;
-        position: absolute;
-        border-radius: 6px;
-        right: 80px;
-      }
-      .footer span {
-        display: inline-block;
-        width: 62px;
-        height: 48px;
-        background: #ccc;
-        font-weight: 900;
-        line-height: 45px;
-        cursor: pointer;
-        text-align: center;
-        position: absolute;
-        right: 10px;
-        border-radius: 6px;
-      }
-      .footer span:hover {
-        color: #fff;
-        background: #999;
-      }
-      #user_face_icon {
-        display: inline-block;
-        /* background: red; */
-        width: 60px;
-        height: 60px;
-        border-radius: 30px;
-        position: absolute;
-        bottom: 6px;
-        left: 14px;
-        cursor: pointer;
-        overflow: hidden;
-      }
-      img {
-        width: 60px;
-        height: 60px;
-      }
-      .content {
-        font-size: 20px;
-        width: 435px;
-        height: 662px;
-        overflow: auto;
-        padding: 5px;
-      }
-      .content li {
-        margin-top: 10px;
-        padding-left: 10px;
-        width: 412px;
-        display: block;
-        clear: both;
-        overflow: hidden;
-      }
-      .content li img {
-        float: left;
-      }
-      .content li span {
-        background: #7cfc00;
-        padding: 10px;
-        border-radius: 10px;
-        float: left;
-        margin: 6px 10px 0 10px;
-        max-width: 310px;
-        border: 1px solid #ccc;
-        box-shadow: 0 0 3px #ccc;
-      }
-      .content li img.imgleft {
-        float: left;
-      }
-      .content li img.imgright {
-        float: right;
-      }
-      .content li span.spanleft {
-        float: left;
+        width: 100%;
+        min-height: 650px;
         background: #fff;
-      }
-      .content li span.spanright {
-        float: right;
-        background: #7cfc00;
-=======
-        padding: 20px;
       }
       #container iframe{
         width: 100%;
-        height: 100%;
->>>>>>> master
+        height: 753px;
+        border: none;
       }
 </style>
