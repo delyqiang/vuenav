d5f5efb38eca29f62df2485ae5da271044560a63
dlq <dailiqiang_6@163.com>
2019-01-16 15:29:07 +0800

    0116优化样式

--- src/App.vue
+++ src/App.vue
@@ -33,4 +33,79 @@ body{
   width: 100%;
   height: 100%;
 }
+
+#app::-webkit-scrollbar{/*滚动条整体部分，其中的属性有width,height,background,border等（就和一个块级元素一样）（位置1）*/
+			width:3px;
+			height:10px;
+	}
+	/* #app::-webkit-scrollbar-button{/*滚动条两端的按钮，可以用display:none让其不显示，也可以添加背景图片，颜色改变显示效果（位置2）*/
+			/* background:#74D334;
+	} */
+	#app::-webkit-scrollbar-track{/*外层轨道，可以用display:none让其不显示，也可以添加背景图片，颜色改变显示效果（位置3）*/
+			background:#fff;
+	}
+	#app::-webkit-scrollbar-track-piece{/*内层轨道，滚动条中间部分（位置4）*/
+			background:#fff;
+	}
+	#app::-webkit-scrollbar-thumb{/*滚动条里面可以拖动的那部分（位置5）*/
+		background:#FFA711;
+		border-radius:4px;
+	}
+	#app::-webkit-scrollbar-corner {/*边角（位置6）*/
+		background:#82AFFF; 
+	}
+	#app::-webkit-scrollbar-resizer  {/*定义右下角拖动块的样式（位置7）*/
+		background:#FF0BEE;
+	}
+
+
+body::-webkit-scrollbar{/*滚动条整体部分，其中的属性有width,height,background,border等（就和一个块级元素一样）（位置1）*/
+			width:5px;
+			height:10px;
+	}
+	/* body::-webkit-scrollbar-button{/*滚动条两端的按钮，可以用display:none让其不显示，也可以添加背景图片，颜色改变显示效果（位置2）*/
+			/* background:#74D334;
+	} */
+	body::-webkit-scrollbar-track{/*外层轨道，可以用display:none让其不显示，也可以添加背景图片，颜色改变显示效果（位置3）*/
+			background:#fff;
+	}
+	body::-webkit-scrollbar-track-piece{/*内层轨道，滚动条中间部分（位置4）*/
+			background:#fff;
+	}
+	body::-webkit-scrollbar-thumb{/*滚动条里面可以拖动的那部分（位置5）*/
+		background:#FFA711;
+		border-radius:4px;
+	}
+	body::-webkit-scrollbar-corner {/*边角（位置6）*/
+		background:#82AFFF; 
+	}
+	body::-webkit-scrollbar-resizer  {/*定义右下角拖动块的样式（位置7）*/
+		background:#FF0BEE;
+	}
+
+
+
+html::-webkit-scrollbar{/*滚动条整体部分，其中的属性有width,height,background,border等（就和一个块级元素一样）（位置1）*/
+			width:5px;
+			height:10px;
+	}
+	/* html::-webkit-scrollbar-button{/*滚动条两端的按钮，可以用display:none让其不显示，也可以添加背景图片，颜色改变显示效果（位置2）*/
+			/* background:#74D334;
+	} */
+	html::-webkit-scrollbar-track{/*外层轨道，可以用display:none让其不显示，也可以添加背景图片，颜色改变显示效果（位置3）*/
+			background:#fff;
+	}
+	html::-webkit-scrollbar-track-piece{/*内层轨道，滚动条中间部分（位置4）*/
+			background:#fff;
+	}
+	html::-webkit-scrollbar-thumb{/*滚动条里面可以拖动的那部分（位置5）*/
+		background:#FFA711;
+		border-radius:4px;
+	}
+	html::-webkit-scrollbar-corner {/*边角（位置6）*/
+		background:#82AFFF; 
+	}
+	html::-webkit-scrollbar-resizer  {/*定义右下角拖动块的样式（位置7）*/
+		background:#FF0BEE;
+	}
 </style>
